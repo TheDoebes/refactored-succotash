09356dce66d5eec89d5b2ad3aea981763daa7d41
TheDoebes <TheDoebes@users.noreply.github.com>
2020-02-25 18:18:56 -0500

    Update UART_RX.v

--- DE2-115/UART_RX.v
+++ DE2-115/UART_RX.v
@@ -1,7 +1,18 @@
-module UART_RX(CLK50MHz, DATA);
-	input CLK50MHz;
+module UART_RX(CLK50MHz, RX, DATA);
+	input CLK50MHz, RX;
 	output [7:0] DATA;
 	
+	reg [17:0] counter;
 	
-	
+	always@(posedge CLK50MHz) begin
+		counter <= counter + 1;
+		if (counter == 18'd153000) 
+			begin
+				counter <= 0;
+				// whatever written here happens on every counter tick
+				
+				
+			end
+			
+	end
 endmodule
