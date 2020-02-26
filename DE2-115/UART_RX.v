a6f637beca240cf85a4a95851dbba4d39d403349
TheDoebes <TheDoebes@users.noreply.github.com>
2020-02-26 15:41:09 -0500

    Add pseudocode to UART module

--- DE2-115/UART_RX.v
+++ DE2-115/UART_RX.v
@@ -1,18 +1,64 @@
 module UART_RX(CLK50MHz, RX, DATA);
 	input CLK50MHz, RX;
 	output [7:0] DATA;
-	
+
+
+	// Internal Circuitry
 	reg [17:0] counter;
-	
-	always@(posedge CLK50MHz) begin
-		counter <= counter + 1;
-		if (counter == 18'd153000) 
-			begin
-				counter <= 0;
-				// whatever written here happens on every counter tick
-				
-				
-			end
-			
+	reg tick;
+	reg [1:0] statemachine;
+
+
+	// RX reader
+	always @(posedge tick) begin
+		case (statemachine)
+			//2'b00 : 
+			//2'b01 :
+			//2'b10 :
+			//2'b11 :
+			default : ;
+		endcase
 	end
+
+
+	// Tick generator
+	always @(posedge CLK50MHz) begin
+		if (counter == 18'b0)
+		begin
+			tick <= 0;
+		end
+
+
+		counter <= counter + 1;
+		if (counter == 18'd153000)
+		begin
+			counter <= 0;
+			tick <= 1;
+		end
+
+	end
+
+	/*PseudoCode
+	 * Step 1:
+		Counter that outputs a 153KHz clock from 50MHz
+			this gives a 9600 baud reciever
+			sampled 16 times AKA 16 clocks per it period
+		Step 2:
+			always block (procedural) based off that counter
+		3:
+			if hi-low transistion then
+			-we know its the start bit
+			-this means wait 1.5 bit periods to sample center of 1st bit
+				wait for 1.5 * 16 = 24 counter ticks
+				for (num bits)
+					sample nth bit - need register array to deserialize these
+					wait 16 ticks
+				repeat
+		- optional: check the parity bit
+		we should expect to see the end bit here
+			send the register array to the audio block
+		
+		go back and wait for another hi-low transistion
+		 
+	 */
 endmodule
--- DE2-115/part1.v
+++ DE2-115/part1.v
@@ -1,8 +1,9 @@
-module part1 (CLOCK_50, CLOCK2_50, KEY, I2C_SCLK, I2C_SDAT, AUD_XCK, 
-		        AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK, AUD_ADCDAT, AUD_DACDAT);
+module part1 (CLOCK_50, CLOCK2_50, KEY, SW, I2C_SCLK, I2C_SDAT, AUD_XCK,
+	AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK, AUD_ADCDAT, AUD_DACDAT);
 
 	input CLOCK_50, CLOCK2_50;
 	input [0:0] KEY;
+	input [1:0] SW;
 	// I2C Audio/Video config interface
 	output I2C_SCLK;
 	inout I2C_SDAT;
@@ -11,74 +12,105 @@ module part1 (CLOCK_50, CLOCK2_50, KEY, I2C_SCLK, I2C_SDAT, AUD_XCK,
 	input AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK;
 	input AUD_ADCDAT;
 	output AUD_DACDAT;
-	
+
 	// Local wires.
 	wire read_ready, write_ready, read, write;
 	wire [23:0] readdata_left, readdata_right;
-	wire [23:0] writedata_left, writedata_right;
+	reg [23:0] writedata_left, writedata_right;
 	wire reset = ~KEY[0];
 
 	/////////////////////////////////
 	// Your code goes here 
 	/////////////////////////////////
-	
-	assign writedata_left = (readdata_left * 2048) /4096;
-	assign writedata_right = (readdata_right * 2048) /4096 ; 
+
+	//	assign writedata_left = readdata_left;//(readdata_left * 2048) /4096;
+	//assign writedata_right = readdata_right;//(readdata_right * 2048) /4096 ; 
 	assign read = read_ready && write_ready;
 	assign write = read_ready && write_ready;
-	
-/////////////////////////////////////////////////////////////////////////////////
-// Audio CODEC interface. 
-//
-// The interface consists of the following wires:
-// read_ready, write_ready - CODEC ready for read/write operation 
-// readdata_left, readdata_right - left and right channel data from the CODEC
-// read - send data from the CODEC (both channels)
-// writedata_left, writedata_right - left and right channel data to the CODEC
-// write - send data to the CODEC (both channels)
-// AUD_* - should connect to top-level entity I/O of the same name.
-//         These signals go directly to the Audio CODEC
-// I2C_* - should connect to top-level entity I/O of the same name.
-//         These signals go directly to the Audio/Video Config module
-/////////////////////////////////////////////////////////////////////////////////
-	clock_generator my_clock_gen(
-		// inputs
-		CLOCK2_50,
-		reset,
 
-		// outputs
-		AUD_XCK
+	always @(*)
+	begin
+		case (SW)
+			2'b01 :
+			begin
+				writedata_left <= (readdata_left * 1024)/1024;
+				writedata_right <= (readdata_right * 1024)/1024; 
+			end
+			2'b10 :
+			begin
+				writedata_left <= (readdata_left * 2048)/2048;
+				writedata_right <= (readdata_right * 2048)/2048; 
+			end
+			2'b11:
+			begin
+				writedata_left <= (readdata_left * 4096)/4096;
+				writedata_right <= (readdata_right * 4096)/4096; 
+			end
+			default :
+			begin
+				writedata_left <= readdata_left;
+				writedata_right <= readdata_right;
+			end
+		endcase
+	end
+	
+	
+	// Instantiate the module for UART recieving from the ardunio
+
+
+
+	/////////////////////////////////////////////////////////////////////////////////
+	// Audio CODEC interface. 
+	//
+	// The interface consists of the following wires:
+	// read_ready, write_ready - CODEC ready for read/write operation 
+	// readdata_left, readdata_right - left and right channel data from the CODEC
+	// read - send data from the CODEC (both channels)
+	// writedata_left, writedata_right - left and right channel data to the CODEC
+	// write - send data to the CODEC (both channels)
+	// AUD_* - should connect to top-level entity I/O of the same name.
+	//         These signals go directly to the Audio CODEC
+	// I2C_* - should connect to top-level entity I/O of the same name.
+	//         These signals go directly to the Audio/Video Config module
+	/////////////////////////////////////////////////////////////////////////////////
+	clock_generator my_clock_gen(
+	// inputs
+	CLOCK2_50,
+	reset,
+
+	// outputs
+	AUD_XCK
 	);
 
 	audio_and_video_config cfg(
-		// Inputs
-		CLOCK_50,
-		reset,
+	// Inputs
+	CLOCK_50,
+	reset,
 
-		// Bidirectionals
-		I2C_SDAT,
-		I2C_SCLK
+	// Bidirectionals
+	I2C_SDAT,
+	I2C_SCLK
 	);
 
 	audio_codec codec(
-		// Inputs
-		CLOCK_50,
-		reset,
+	// Inputs
+	CLOCK_50,
+	reset,
 
-		read,	write,
-		writedata_left, writedata_right,
+	read,	write,
+	writedata_left, writedata_right,
 
-		AUD_ADCDAT,
+	AUD_ADCDAT,
 
-		// Bidirectionals
-		AUD_BCLK,
-		AUD_ADCLRCK,
-		AUD_DACLRCK,
+	// Bidirectionals
+	AUD_BCLK,
+	AUD_ADCLRCK,
+	AUD_DACLRCK,
 
-		// Outputs
-		read_ready, write_ready,
-		readdata_left, readdata_right,
-		AUD_DACDAT
+	// Outputs
+	read_ready, write_ready,
+	readdata_left, readdata_right,
+	AUD_DACDAT
 	);
 
 endmodule
