6dd058fc5605e9644adef40c62e55ffe78be32f5
TheDoebes <TheDoebes@users.noreply.github.com>
2020-03-03 17:05:35 -0500

    Finish UART_RX reset block

--- DE2-115/UART_RX.v
+++ DE2-115/UART_RX.v
@@ -1,19 +1,28 @@
 module UART_RX(CLK50MHz, RESET, RX, DATA);
-	input CLK50MHz, RX;
+	// Handles recieving UART data
+	// Assumes 8-bit words, even parity bit, 9600 Baud
+	// DATA[0] is the first bit, DATA[7] is the last
+	// If not valid signal detected, should output all zeros
+	
+	// I/O
+	input CLK50MHz; // Control clock from FPGA
+	input RX; // UART RX line from device
 	input RESET; // Active low reset signal
-	output reg [7:0] DATA;
+	output reg [7:0] DATA; // Output data to FPGA
 
 
 	// Internal Circuitry
+	
+	// Counter variables
 	reg [17:0] counter; // Used to generate tick from 50MHz clock
 	reg tick; // 153KHz Sampling Clock signal
+	
+	//State Machine Variables
 	reg [3:0] statemachine; // StateMachine state counter
-
+	reg [4:0] pointer; // used to count tick before resampling
 	reg [7:0] cache; // Caching samples before sending full words to DATA
 	reg [2:0] cacheIndex; // Track which bit we are sampling into cache	
-
-	reg [4:0] pointer; // used to count tick before resampling
-	reg parityBit;
+	reg parityBit; // store the parity of the cache register
 
 
 	// RX reader - Samples bitstream
@@ -21,22 +30,30 @@ module UART_RX(CLK50MHz, RESET, RX, DATA);
 	begin
 		if (RESET == 0)
 			begin
-				;
+				statemachine <= 4'd3;	// Send the statemachine to the stop bit
+				pointer <= 0; // Set the number of ticks counted to zero
+				cache <= 0; // Empty the cache so old samples are not sent
+				cacheIndex <= 0; // Make sure we start refilling the cache from zero
+				partiyBit <= 0; // Set correct parity for empty cache
+				
+				DATA <= 8'd0; // Empty the output register
+				
 			end
 		else
-			begin
+			begin	// TODO Check if there's a bug where state transitions don't happen on n%16 tick intervals
 				case (statemachine) // Interpret the RX line
 
 					// Idle state
-					4'd0	: // Detect start bit
+					4'd0	: 
 					begin
-						if (RX == 0)
+						if (RX == 0 || pointer != 0) // Detect start bit or if already found start bit
 							begin
 								pointer <= pointer + 1;
 								if (pointer == 5'd24)
 								begin
 									statemachine <= statemachine + 1;
 									pointer <= 0;
+									cacheIndex <= 0;
 								end
 							end
 						else
@@ -61,6 +78,7 @@ module UART_RX(CLK50MHz, RESET, RX, DATA);
 										begin
 											cacheIndex <= 0; // Reset for next loop and
 											statemachine <= 4'd2; // then go to the next state
+											pointer <= 0;
 										end
 									else // Then move to the next bit
 										begin
@@ -71,22 +89,25 @@ module UART_RX(CLK50MHz, RESET, RX, DATA);
 							end
 					end
 
-					// Check the Parity bit
+					// Check the Parity bit to ensure even parity
 					4'd2	:
 					begin
 						if (pointer == 0) // If we haven't sampled this bit yet, sample it
 							begin
 
-								// TODO fix this trash vvv
-								parityBit <= cache[0]^cache[1]^cache[2]^cache[3]^cache[4]^cache[5]^cache[6]^cache[7];
+								
 								pointer <= pointer + 1; // and mark it as such
-
-								if (parityBit == RX)
+								
+								// TODO fix this trash
+								parityBit <= cache[0]^cache[1]^cache[2]^cache[3]^cache[4]^cache[5]^cache[6]^cache[7]; // calculate the parity bit from the cache
+								
+								if (parityBit == RX) // If they match, the data is probably good so tranmit the update
 								begin
 									DATA <= cache;
 								end
+								// Otherwise leave the old data bit
 							end
-						else // otherwise we have sampled it already
+						else // otherwise we have sampled it already and we're just waiting for the next bit
 							begin
 								pointer <= pointer + 1;
 
